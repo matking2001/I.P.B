4593b881240f637c6d7413184f6c48ba6ba2d4b3
mateo <balzamateo5@gmail.com>
2021-02-09 17:32:42 -0500

    Inicio de sesión terminado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', sign_up: 'register' }
   resources :asistentes
-  root to: 'asitente#index'
+  root to: 'asistentes#index'
 
 end
